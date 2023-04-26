ed90f79d5c215e4f6a8158bbf1bd459b3c5b2a69
Akash <akashbytes@gmail.com>
2023-04-27 01:06:50 +0530

    created a search filter function and top rating function

--- reactWparcel/src/Components/swiggyComponents/BodyComponents/CardCounter.js
+++ reactWparcel/src/Components/swiggyComponents/BodyComponents/CardCounter.js
@@ -4,7 +4,8 @@ import Cards from "./Cards.js";
 
 const CardCounter = () => {
   const [allRestaurants, setNewRestaurants] = useState([]);
-//   console.log(allRestaurants);
+  const [searchText, setSearchtext] = useState("");
+  //   console.log(allRestaurants);
 
   useEffect(() => {
     getRestaurants();
@@ -18,33 +19,70 @@ const CardCounter = () => {
     // console.log(jsonData)
     setNewRestaurants(jsonData?.data?.cards);
   }
+  // on serach  filter function
+  function filterItems(searchText, allRestaurants) {
+    const filterByName = allRestaurants.filter((Cards) =>
+      Cards?.data?.data?.name.toLowerCase().includes(searchText)
+    );
+    return filterByName;
+  }
 
+  //  Top Rated Cards
+  function TopRatedFood(allRestaurants) {
+    const topRatings = allRestaurants.filter(
+      (ratings) => ratings.data.data.avgRating > 3
+    );
+    // topRatings;
+    console.log(topRatings)
+  }
   return (
     <div className="card-counter-box">
       <div className="search-header">
         {/* Search input filter buttons  */}
         <div className="sreach-box">
-          <input type="text" placeholder="Search" className="input-search" />
-          <button type="submit" className="input-btn">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input-search"
+            value={searchText}
+            onChange={(e) => setSearchtext(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="input-btn"
+            onClick={() => {
+              const filterCards = filterItems(searchText, allRestaurants);
+              // console.log(filterCards)
+              setNewRestaurants(filterCards);
+            }}
+          >
             search
           </button>
         </div>
         <div className="filter-btn-box">
           <button className="filter-btns">relevence</button>
           <button className="filter-btns">Discounts</button>
-          <button className="filter-btns">top ratings</button>
+          <button
+            className="filter-btns"
+            onClick={() => {
+              const TopRatedcards = TopRatedFood(allRestaurants);
+              // setNewRestaurants(TopRatedcards);
+            }}
+          >
+            top ratings
+          </button>
         </div>
       </div>
       {/* resturant cards  */}
       <div className="cards-box">
-
-      {
-        allRestaurants?.map((resturant )=>{
-            {/* console.log(resturant ) */}
-            return(<Cards resturantList={resturant} key={resturant.data.data.id}/>)
-        }) 
-      }
-      
+        {allRestaurants?.map((resturant) => {
+          {
+            /* console.log(resturant ) */
+          }
+          return (
+            <Cards resturantList={resturant} key={resturant.data.data.id} />
+          );
+        })}
       </div>
     </div>
   );
