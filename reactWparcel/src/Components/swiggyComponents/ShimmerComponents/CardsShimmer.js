786d52237970a473207af07b4688a2b841ee8e70
Akash <akashbytes@gmail.com>
2023-04-29 00:36:26 +0530

    created shimmer for cards

--- reactWparcel/src/Components/swiggyComponents/ShimmerComponents/CardsShimmer.js
+++ reactWparcel/src/Components/swiggyComponents/ShimmerComponents/CardsShimmer.js
@@ -7,17 +7,11 @@ const CardsShimmer = ()=>{
         <figure className="shimmer-image"></figure>
         <div className="shimmer-data-containor">
         <figcaption className="shimmer-shop-name"></figcaption>
-        <p className="shimmer-shimmer-cuisines-box"></p>
+        <p className="shimmer-cuisines-box"></p>
+        <p className="shimmer-food-text"></p>
         <p className="shimmer-food-text"></p>
         <div className="shimmer-RTP-box">
-            {/* <span className="shimmer-mrp-text"></span>
-            <span className="shimmer-mrp-text"></span>
-            <span className="shimmer-mrp-text"></span> */}
         </div>
-        {/* <div className="shimmer-btn-box">
-            <p className="shimmer-offer-text"></p>
-            <p className="shimmer-offer-text"></p>
-        </div> */}
         </div>
       </div>
     </>  )
