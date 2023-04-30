0ea78d9c9a396679914ebc7adadd886cad061061
Akash <akashbytes@gmail.com>
2023-04-30 14:23:34 +0530

    install react router and used in app.js

--- reactWparcel/App.js
+++ reactWparcel/App.js
@@ -1,9 +1,13 @@
-// import React from "react"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 // import Header from './src/Components/AssignmentComponents/Assignment3';
 // import First from  './src/Components/AssignmentComponents/First' ;
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import NavHeader from './src/Components/swiggyComponents/HeaderComponents/NavHeader';
 import CardCounter from './src/Components/swiggyComponents/BodyComponents/CardCounter';
+import About from "./src/Components/swiggyComponents/BodyComponents/AboutComponets/About"
+import ErrorMess from './src/Components/swiggyComponents/FooterComponents/ErrorElement';
 
 
 const App = ()=> {
@@ -14,7 +18,28 @@ const App = ()=> {
 
     <NavHeader/>
     <CardCounter></CardCounter>
+    <About/>
     </>)
 }
 
-export default App
+const appRouter= createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+        errorElement:<ErrorMess/>
+    },
+    {
+        path:"about",
+        element:<About/>,
+    }
+])
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={ appRouter} />
+   
+  </React.StrictMode>
+);
