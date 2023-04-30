46df4129dd0b784cd3c2a5463b6c4fff7be8096c
Akash <akashbytes@gmail.com>
2023-04-30 14:29:53 +0530

    edited in nav

--- reactWparcel/src/Components/swiggyComponents/HeaderComponents/NavHeader.js
+++ reactWparcel/src/Components/swiggyComponents/HeaderComponents/NavHeader.js
@@ -1,33 +1,41 @@
-import React, { useState } from "react"
-import "./navHeader.css"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navHeader.css";
 
-const NavHeader = ()=>{
-const [userLog , setUserLog] = useState(false)
+const NavHeader = () => {
+  const [userLog, setUserLog] = useState(false);
 
-const LoginHandler = () => {
-    if(userLog == false){
-        setUserLog(true)
-        console.log(true)
-    }else if (userLog == true){
-        setUserLog(false)
-        console.log(false)
+  const LoginHandler = () => {
+    if (userLog == false) {
+      setUserLog(true);
+      console.log(true);
+    } else if (userLog == true) {
+      setUserLog(false);
+      console.log(false);
     }
-}
+  };
 
+  return (
+    <>
+      <nav className="nav-box">
+        <div className="logo-box">
+          <span className="logo-name">Foodies</span>
+        </div>
+        <div className="link-box">
+          <ul>
+            <li><Link to="/">home</Link> </li>
+            <li><Link to="about">about</Link> </li>
+            <li><Link to="discount">discount</Link></li>
+            <li><Link to="'contact">  cart<span> </span> <i className="fa-solid fa-cart-shopping"></i></Link></li>
+          </ul>
+        </div>
+        {/* <button className={!userLog == true ? "log-btn" : "log-btn login-btn"} onClick={()=>LoginHandler()}>{!userLog == true ? "log in" : "log out"}</button> */}
+        <div className="togle-btn">
+          <i className="fa-solid fa-bars"></i>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-    return(<>
-        <nav className="nav-box">
-            <div className="logo-box"><span className="logo-name">Foodies</span></div>
-            <div className="link-box"><ul>
-                <li>home</li>
-                <li>about</li>
-                <li>discount</li>
-                <li>cart<span>{" "}</span><i className="fa-solid fa-cart-shopping"></i></li>
-            </ul></div>
-       {/* <button className={!userLog == true ? "log-btn" : "log-btn login-btn"} onClick={()=>LoginHandler()}>{!userLog == true ? "log in" : "log out"}</button> */}
-            <div className="togle-btn"><i className="fa-solid fa-bars"></i></div>
-        </nav>
-    </>)
-}
-
-export default NavHeader
+export default NavHeader;
